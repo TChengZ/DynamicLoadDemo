325d5954f6ff28049daace88e3170c6278527dd6
chenzujie <chenzujie@91yong.com>
2016-08-25 11:47:20 +0800

    增加插件判断

--- app/src/main/java/com/jc/dynamicloaddemo/MainActivity.java
+++ app/src/main/java/com/jc/dynamicloaddemo/MainActivity.java
@@ -1,12 +1,14 @@
 package com.jc.dynamicloaddemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Environment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.ryg.dynamicload.internal.DLIntent;
 import com.ryg.dynamicload.internal.DLPluginManager;
@@ -31,6 +33,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
     public void onClick(View view) {
         if(view == mBtnLaunchActivity){
             String pluginApkPath = Environment.getExternalStorageDirectory() + File.separator + "plugin.apk";
+            File file = new File(pluginApkPath);
+            if(!file.exists()){
+                Toast.makeText(MainActivity.this, "插件apk不存在，请在sd卡目录放plugin.apk作为插件", Toast.LENGTH_SHORT).show();
+                return;
+            }
             DLPluginManager pluginManager = DLPluginManager.getInstance(BaseApplicaiton.getInstance());
             DLPluginPackage dlPluginPackage = pluginManager.loadApk(pluginApkPath);
             pluginManager.startPluginActivity(this, new DLIntent(dlPluginPackage.packageName, dlPluginPackage.defaultActivity));
